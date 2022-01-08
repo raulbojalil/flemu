dbcac7bd37b448bffc8f2c9416406757558df4cf
raulbojalil <raulbojalil@gmail.com>
2022-01-08 14:40:24 -0600

    Added simple server-backed savestate saving

--- backend/app.js
+++ backend/app.js
@@ -2,124 +2,28 @@ const PORT = process.env.PORT || 5000;
 
 const express = require('express');
 const app = express();
-const fs = require('fs');
 const cors = require('cors');
-const path = require('path');
-const client = require('https');
-
-const screenscraper = require('./screenscraper');
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(cors());
 
-let _gameDb = {};
-let _gameFolders = {};
+const filemanager = require('./filemanager');
 
-fs.readFile('./game_db.json', 'utf8', (err, data) => {
-  _gameDb = JSON.parse(data);
-});
-
-fs.readFile('./game_paths.json', 'utf8', (err, data) => {
-  _gameFolders = JSON.parse(data);
-});
-
-const getGameIdFromName = (system, name) => {
-
-  const nameToLowerCase = name.toLowerCase().replace(", the", "");
-  const games = _gameDb[system].sort((a,b) => b.name.length - a.name.length);
-
-  for(var i=0; i < games.length; i++) {
-
-    if(nameToLowerCase.includes(games[i].name.toLowerCase())) {
-      const gameId = games[i].id;
-      console.log(`Found game id: ${gameId}: ${games[i].name}`);
-      return gameId;
-    }
-  }
-
-  return undefined;
-}
-
-app.get('/filemanager/systems', (req, res) => {
-  fs.readFile('./systems.json', 'utf8', (err, data) => {
-     res.json(JSON.parse(data));
-  });
-});
-
-app.get('/filemanager/description', (req, res) => {
-  
-  const system = req.query.system;
-  const cachedDescriptionsFolder = __dirname + '/descriptions/' + system;
-  const cachedFilename = path.join(cachedDescriptionsFolder, req.query.name + '.json');
-
-  if(!fs.existsSync(cachedDescriptionsFolder)) {
-    fs.mkdirSync(cachedDescriptionsFolder, { recursive: true });
-  }
-
-  if (fs.existsSync(cachedFilename)) {
-    fs.readFile(cachedFilename, 'utf8', (err, data) => {
-      res.json(JSON.parse(data));
-    });
-  } else {
-    const gameId = getGameIdFromName(system, req.query.name);
-
-    if(!gameId) {
-      res.json([]);
-      return;
-    }
-
-    screenscraper.downloadGameDescriptions(gameId, cachedFilename).then(() => {
-      fs.readFile(cachedFilename, 'utf8', (err, data) => {
-        res.json(JSON.parse(data));
-      });
-    });
-  }
-
-});
-
-app.get('/filemanager/image', (req, res) => {
-
-  const system = req.query.system;
-  const fallbackImage = __dirname + '/notfound.jpg';
-
-  const cachedImagesFolder = __dirname + '/images/' + system;
-  const cachedFilename = path.join(cachedImagesFolder, req.query.name + '.jpg');
-
-  if(!fs.existsSync(cachedImagesFolder)) {
-    fs.mkdirSync(cachedImagesFolder, { recursive: true });
-  }
-
-  if (fs.existsSync(cachedFilename)) {
-    res.sendFile(cachedFilename);
-    return;
-  }
-
-  const gameId = getGameIdFromName(system, req.query.name);
-
-  if(!gameId) {
-    res.sendFile(fallbackImage);
-    return;
-  }
-
-  screenscraper.downloadGameImage(gameId, cachedFilename).then(() => {
-    res.sendFile(cachedFilename);
-  });
-
-});
-
-app.get('/filemanager/download', (req, res) => {
-  const folder = _gameFolders[req.query.folder];  
-  res.sendFile(path.join(folder, req.query.filename));
-});
-
-app.get('/filemanager/list', (req, res) => {
-  const folder = _gameFolders[req.query.folder];
-  const files = fs.readdirSync(folder);
-
-  res.json(files);
-
-});
+//GET /filemanager/systems
+app.get('/filemanager/systems', filemanager.getSystems);
+//GET /filemanager/description?system=&name=
+app.get('/filemanager/description', filemanager.getDescription);
+//GET /filemanager/image?system=&name=
+app.get('/filemanager/image', filemanager.getImage);
+//GET /filemanager/download?folder=&filename=
+app.get('/filemanager/download', filemanager.downloadFile);
+//GET /filemanager/list?folder=
+app.get('/filemanager/list', filemanager.listFolderContents);
+//POST /filemanager/savestate { system, name, state, screenshot }
+app.post('/filemanager/savestate', filemanager.saveState);
+//GET /filemanager/loadstate?system=&name=
+app.get('/filemanager/loadstate', filemanager.loadState);
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`)
---
+++ backend/filemanager.js
@@ -0,0 +1,153 @@
+const fs = require('fs');
+const path = require('path');
+
+const screenscraper = require('./services/screenscraper');
+
+let _gameDb = {};
+let _gameFolders = {};
+let _systems = {};
+
+fs.readFile('./game_db.json', 'utf8', (err, data) => {
+  _gameDb = JSON.parse(data);
+});
+
+fs.readFile('./game_paths.json', 'utf8', (err, data) => {
+  _gameFolders = JSON.parse(data);
+});
+
+fs.readFile('./systems.json', 'utf8', (err, data) => {
+  _systems = JSON.parse(data);
+});
+
+const getGameIdFromName = (system, name) => {
+
+  const nameToLowerCase = name.toLowerCase().replace(", the", "");
+  const games = _gameDb[system].sort((a,b) => b.name.length - a.name.length);
+
+  for(var i=0; i < games.length; i++) {
+
+    if(nameToLowerCase.includes(games[i].name.toLowerCase())) {
+      const gameId = games[i].id;
+      console.log(`Found game id: ${gameId}: ${games[i].name}`);
+      return gameId;
+    }
+  }
+
+  return undefined;
+}
+
+exports.getSystems = (req, res) => {
+  res.json(_systems);
+};
+
+exports.getDescription = (req, res) => {
+  
+  const system = req.query.system;
+  const cachedDescriptionsFolder = __dirname + '/descriptions/' + system;
+  const cachedFilename = path.join(cachedDescriptionsFolder, req.query.name + '.json');
+
+  if(!fs.existsSync(cachedDescriptionsFolder)) {
+    fs.mkdirSync(cachedDescriptionsFolder, { recursive: true });
+  }
+
+  if (fs.existsSync(cachedFilename)) {
+    fs.readFile(cachedFilename, 'utf8', (err, data) => {
+      res.json(JSON.parse(data));
+    });
+  } else {
+    const gameId = getGameIdFromName(system, req.query.name);
+
+    if(!gameId) {
+      res.json([]);
+      return;
+    }
+
+    screenscraper.downloadGameDescriptions(gameId, cachedFilename).then(() => {
+      fs.readFile(cachedFilename, 'utf8', (err, data) => {
+        res.json(JSON.parse(data));
+      });
+    });
+  }
+
+};
+
+exports.getImage = (req, res) => {
+
+  const system = req.query.system;
+  const fallbackImage = __dirname + '/notfound.jpg';
+
+  const cachedImagesFolder = __dirname + '/images/' + system;
+  const cachedFilename = path.join(cachedImagesFolder, req.query.name + '.jpg');
+
+  if(!fs.existsSync(cachedImagesFolder)) {
+    fs.mkdirSync(cachedImagesFolder, { recursive: true });
+  }
+
+  if (fs.existsSync(cachedFilename)) {
+    res.sendFile(cachedFilename);
+    return;
+  }
+
+  const gameId = getGameIdFromName(system, req.query.name);
+
+  if(!gameId) {
+    res.sendFile(fallbackImage);
+    return;
+  }
+
+  screenscraper.downloadGameImage(gameId, cachedFilename).then(() => {
+    res.sendFile(cachedFilename);
+  });
+
+};
+
+exports.downloadFile = (req, res) => {
+  const folder = _gameFolders[req.query.folder];  
+  res.sendFile(path.join(folder, req.query.filename));
+};
+
+exports.listFolderContents = (req, res) => {
+  const folder = _gameFolders[req.query.folder];
+  const files = fs.readdirSync(folder);
+  res.json(files);
+};
+
+exports.saveState = (req, res) => {
+  const folder = req.body.system;
+  const state = req.body.state;
+
+  //TODO: Save the screenshot
+  const screenshot = req.body.screenshot;
+  
+  const savesFolder = __dirname + '/saves/' + folder;
+  const saveState = path.join(savesFolder, req.body.name + '.sav');
+
+  if(!fs.existsSync(savesFolder)) {
+    fs.mkdirSync(savesFolder, { recursive: true });
+  }
+
+  fs.writeFile(saveState, state, 'utf-8', () => {
+     res.json({});
+  });
+}
+
+exports.loadState = (req, res) => {
+  const folder = req.query.system;
+  
+  const savesFolder = __dirname + '/saves/' + folder;
+  const saveState = path.join(savesFolder, req.query.name + '.sav');
+
+  if(!fs.existsSync(savesFolder)) {
+    fs.mkdirSync(savesFolder, { recursive: true });
+  }
+
+  if(!fs.existsSync(saveState)) {
+    res.json({ state: null });
+    return;
+  }
+
+  fs.readFile(saveState, 'utf8', (err, data) => {
+     res.json({ state: data });
+  });
+
+}
